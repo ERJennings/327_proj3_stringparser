9a0135474d08a8e91821f55d238c8c3f5d06e6c2
ejennings <erjennings13@gmail.com>
2019-11-01 22:32:40 -0700

    First commit

--- StringParserClass.cpp
+++ StringParserClass.cpp
@@ -7,6 +7,15 @@
 
 #include <string>
 #include <string.h>
+#include <vector>
+#include "../327_proj3_test/includes/StringParserClass.h"
 
 //TODO Fill in
 
+int setTags(const char *pStart, const char *pEnd) {
+	return 0;
+}
+
+int getDataBetweenTags(char *pDataToSearchThru, std::vector<std::string> &myVector) {
+	return 0;
+}
