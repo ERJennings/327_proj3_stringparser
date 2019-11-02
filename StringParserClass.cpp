17866222494c291021da0328d008378274be0dba
Eric Jennings <erjennings13@gmail.com>
2019-11-02 13:39:29 -0700

    Everything but getData

--- StringParserClass.cpp
+++ StringParserClass.cpp
@@ -9,13 +9,70 @@
 #include <string.h>
 #include <vector>
 #include "../327_proj3_test/includes/StringParserClass.h"
+#include "../327_proj3_test/includes/constants.h"
 
 //TODO Fill in
 
-int setTags(const char *pStart, const char *pEnd) {
-	return 0;
-}
+using namespace KP_StringParserClass;
 
-int getDataBetweenTags(char *pDataToSearchThru, std::vector<std::string> &myVector) {
-	return 0;
-}
+
+
+
+		//dont forget to initialize member variables
+		StringParserClass::StringParserClass() {
+			this-> pStartTag = NULL;
+			this-> pEndTag = NULL;
+			this-> areTagsSet = false;
+		}
+
+		//call cleanup to release any allocated memory
+		StringParserClass::~StringParserClass() {
+			cleanup();
+		}
+
+		void StringParserClass::cleanup(){
+			if (pStartTag)
+				delete[] pStartTag;
+
+			if (pEndTag)
+				delete[] pEndTag;
+		}
+
+		//these are the start tag and the end tags that we want to find,
+		//presumably the data of interest is between them, please make a
+		//COPY of what pStartTag and pEndTag point to.  In other words
+		//DO NOT SET pStartTag = pStart
+		//returns:
+		//SUCCESS
+		//ERROR_TAGS_NULL if either pStart or pEnd is null
+		int StringParserClass::setTags(const char *pStart, const char *pEnd) {
+			if (pStart == NULL || pEnd == NULL) {
+				return ERROR_TAGS_NULL;
+			}
+
+			int startLen = strlen(pStart);
+			int endLen = strlen(pEnd);
+
+			pStartTag = new char[startLen + 1];
+			pEndTag = new char[endLen + 1];
+
+			strncpy(pStartTag, pStart, startLen);
+			strncpy(pEndTag, pEnd, endLen);
+
+			return SUCCESS;
+		}
+
+		//First clears myVector
+		//going to search thru pDataToSearchThru, looking for info bracketed by
+		//pStartTag and pEndTag, will add that info only to myVector
+		//returns
+		//SUCCESS  finished searching for data between tags, results in myVector (0 or more entries)
+		//ERROR_TAGS_NULL if either pStart or pEnd is null
+		//ERROR_DATA_NULL pDataToSearchThru is null
+		int StringParserClass::getDataBetweenTags(char *pDataToSearchThru, std::vector<std::string> &myVector) {
+			return 0;
+		}
+
+
+
+
